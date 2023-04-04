67043a24106d21872e11c0b62d48d04ba72cbc55
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-04-04 17:54:44 +0200

    commit to deploy

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,8 +4,6 @@ import Banner from "../../components/Banner";
 import Main from "../../components/Main";
 
 const Home = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
   return (
     <div>
       <Banner />
