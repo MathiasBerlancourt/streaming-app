e5e2468f670fe1566c1b8d3970a3142cbe6b87bc
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-04-06 01:32:48 +0200

    auth context OK,pages sgnin signout account wip firebase fix

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -10,7 +10,7 @@ const Signup = () => {
         alt="signupBackground"
       />
       <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
-      <div className="fixed w-full px-4 py-24 z-50"></div>
+      <div className="fixed w-full px-4 py-24 z-30"></div>
     </div>
   );
 };
