b48cbea9aa63ecbc6d36697b752d2a2813c5be30
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-04-18 19:25:38 +0200

    add remove movie from fav list fix

--- src/components/SavedMovies.jsx
+++ src/components/SavedMovies.jsx
@@ -2,19 +2,37 @@ import { useEffect, useState } from "react";
 import { AiFillLike } from "react-icons/ai";
 import { UserAuth } from "../context/AuthContext";
 import { db } from "../firebase";
-import { useMoviesContext } from "../context/MoviesContext";
+
 import { doc, onSnapshot } from "firebase/firestore";
+import { BsFillTrash3Fill } from "react-icons/bs";
+import { toast } from "react-toastify";
+import { IoTrashBinSharp } from "react-icons/io5";
+import { arrayRemove, updateDoc } from "firebase/firestore";
+
 const SavedMovies = () => {
   const [movies, setMovies] = useState([]);
   const { user } = UserAuth();
-  const {
-    setShowMovieInfosModal,
+  const removeMovieNotify = (movie) =>
+    toast.info(
+      `${movie?.title} a été retiré de votre liste ! `,
+      { icon: IoTrashBinSharp },
+      { toastId: movie?.id }
+    );
 
-    setSelectedMovie,
-  } = useMoviesContext();
-  const handleCardClick = (movie) => {
-    setSelectedMovie(movie);
-    setShowMovieInfosModal(true);
+  const removeMovie = async (movie) => {
+    try {
+      await updateDoc(doc(db, "users", `${user?.email}`), {
+        savedMovies: arrayRemove({
+          id: movie.id,
+          title: movie.title,
+          backdrop_path: movie.backdrop_path,
+          vote_average: movie.vote_average,
+        }),
+      });
+      removeMovieNotify(movie);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -26,6 +44,7 @@ const SavedMovies = () => {
     <div className="moviesRow">
       {movies?.map((movie) => (
         <div
+          key={movie.id}
           className="  text-transparent relative flex-none w-80  mx-1 rounded-lg  object-cover shadow-md transition duration-500 ease-in-out transform hover:scale-125 hover:z-30  hover:text-white
       "
         >
@@ -37,9 +56,13 @@ const SavedMovies = () => {
           ) : (
             <span className="text-white">No poster available</span>
           )}
-
+          <BsFillTrash3Fill
+            onClick={() => removeMovie(movie)}
+            className="absolute bottom-[50%] right-5 text-xl "
+          />
           <div className="absolute bottom-4 left-10">
             <h2>{movie.title}</h2>
+
             <div className="flex items-center ">
               <AiFillLike /> <p>{movie.vote_average}</p>
             </div>
